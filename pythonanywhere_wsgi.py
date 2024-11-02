047ec131f6273dc2bb472cae6addbc4fdb7f7356
Zayar Naing <125944400+MgZayarNaing@users.noreply.github.com>
2024-11-02 13:06:59 +0630

    Update pythonanywhere_wsgi.py

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,13 +1,12 @@
-# wsgi.py
-
-
 # +++++++++++ DJANGO +++++++++++
 # To use your own django app use code like this:
 import os
 import sys
+
 path = '/home/aungaung321/django_blog'
 if path not in sys.path:
-sys.path.append(path)
+  sys.path.append(path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'django_blog.settings'
-# then:
-from django.core.wsgi import get_wsgi_
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()

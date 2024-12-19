e1c1057ebbeb7c6840576574941e6bb4e5ac76ec
Zayar Naing <125944400+MgZayarNaing@users.noreply.github.com>
2024-12-19 11:25:19 +0630

    Update namecheap_wsgi.py

--- namecheap_wsgi.py
+++ namecheap_wsgi.py
@@ -1,1 +1,25 @@
 
+"""
+WSGI config for myProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
+from static_ranges import Ranges
+from dj_static import Cling, MediaCling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coin.settings')
+
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+    
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
